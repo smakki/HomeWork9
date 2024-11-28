4fd692593fecf22ad17f665533c793468851cc3b
belan <smakki.92@gmail.com>
2024-11-28 17:21:38 +0300

    initial commit

---
+++ HomeWork9/BinaryTree.cs
@@ -0,0 +1,66 @@
+﻿namespace HomeWork9;
+
+internal class BinaryTree
+{
+    private List<TreeNode> Nodes { get; } = new();
+    private TreeNode? RootNode { get; set; }
+
+    public void Add(string textValue, int value)
+    {
+        var node = new TreeNode { Value = value, TextValue = textValue };
+        Nodes.Add(node);
+        if (RootNode == null) RootNode = node;
+        AddNode(RootNode, node);
+    }
+
+    public void AddNode(TreeNode root, TreeNode node)
+    {
+        if (root == node) return;
+
+        if (node.Value < root.Value)
+        {
+            if (root.Left == null)
+                root.Left = node;
+            else
+                AddNode(root.Left, node);
+        }
+        else
+        {
+            if (root.Right == null)
+                root.Right = node;
+            else
+                AddNode(root.Right, node);
+        }
+    }
+
+    public static TreeNode? FindNode(BinaryTree tree, int value)
+    {
+        if (tree.RootNode == null) return null;
+        return tree.RootNode.Value == value ? tree.RootNode : FindNodeByValue(tree.RootNode, value);
+    }
+    public static TreeNode? FindNodeByValue(TreeNode root, int value)
+    {
+        if (root.Value == value) return root;
+        return root.Value > value ? FindNodeByValue(root.Left, value)! : FindNodeByValue(root.Right, value)!;
+    }
+
+    public static void TraverseTree(BinaryTree tree)
+    {
+        if (tree.RootNode != null) Traverse(tree.RootNode);
+    }
+
+    private static void Traverse(TreeNode originNode)
+    {
+        if (originNode.Left != null) Traverse(originNode.Left);
+        Console.WriteLine("Employee: {0}    salary:{1}", originNode.TextValue, originNode.Value);
+        if (originNode.Right != null) Traverse(originNode.Right);
+    }
+
+    internal class TreeNode
+    {
+        public string TextValue { get; set; }
+        public int Value { get; set; }
+        public TreeNode? Left { get; set; }
+        public TreeNode? Right { get; set; }
+    }
+}
---
+++ HomeWork9/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace HomeWork9
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            BinaryTree tree = new BinaryTree();
+            var first = true;
+            while (true)
+            {
+                if (first)
+                {
+                    // var tree = ManualEntering();
+                    tree = AutoCreate();
+                    BinaryTree.TraverseTree(tree);
+                    FindEmployeeBySalary(tree);
+                    first = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter 0 to go to the beginning of the program or 1 to search for salary");
+                    var input = Console.ReadLine();
+                    if (input == "0")
+                    {
+                        tree = AutoCreate();
+                        BinaryTree.TraverseTree(tree);
+                        FindEmployeeBySalary(tree);
+                    } else if (input == "1")
+                    {
+                        FindEmployeeBySalary(tree);
+                    }
+                }
+            }
+        }
+
+        public static BinaryTree ManualEntering()
+        {
+            var tree = new BinaryTree();
+            while (true)
+            {
+                Console.WriteLine("Please enter the employee's name");
+                String employee = Console.ReadLine();
+                if (employee == "") break;
+                while (true)
+                {
+                    Console.WriteLine("Please enter the salary");
+                    String salaryText = Console.ReadLine();
+                    if (!int.TryParse(salaryText, out int salary))
+                    {
+                        Console.WriteLine("this is not integer");
+                        continue;
+                    }
+                    tree.Add(employee, salary);
+                    break;
+                }
+
+            }
+            return tree;
+        }
+
+        public static BinaryTree AutoCreate()
+        {
+            var tree = new BinaryTree();
+            var random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                tree.Add(i.ToString(), random.Next(100));
+            }
+            return tree;
+        }
+
+        public static void FindEmployeeBySalary(BinaryTree tree)
+        {
+           Console.WriteLine("enter salary to search");
+           var input = Console.ReadLine();
+           if (input == "") return;
+           if (int.TryParse(input, out int salary))
+           {
+               var result = BinaryTree.FindNode(tree, salary);
+               if (result != null)
+               {
+                   Console.WriteLine($"employee with the specified salary : {result.TextValue}");
+               }
+               else
+               {
+                   Console.WriteLine("employee doesn't exist");
+               }
+           }
+        }
+    }
+}
